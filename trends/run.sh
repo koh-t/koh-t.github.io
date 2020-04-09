2ca4760787a8c32fb8498c298d5208260c0112ac
koh <koh0927@gmail.com>
2020-04-09 13:34:55 +0900

    web update

--- trends/run.sh
+++ trends/run.sh
@@ -1,9 +1,10 @@
 !#!/bin/bash
 echo "update covid trends!"
 cd /Users/koh/Dropbox/Proj-web/koh-t.github.io/trends
+
 source activate pt_env
 python main.py
-conda deactivate pt_env
+conda deactivate
 
 git add .
 git commit -m "web update"
