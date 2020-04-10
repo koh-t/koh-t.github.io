bad9d59bd2963d3f5c930a148f8997819552b927
koh <koh0927@gmail.com>
2020-04-10 17:36:49 +0900

    web update

--- trends/run.sh
+++ trends/run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-echo "update covid trends!"
 cd /Users/koh/Dropbox/Proj-web/koh-t.github.io/trends
-
-source activate pt_env
-python main.py
-conda deactivate
-
+/Users/koh/anaconda3/envs/pt_env/bin/python main.py
 git add .
 git commit -m "web update"
 git push origin master
-
 exit 0
