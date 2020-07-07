2b09b0406d3bb72e25cc184b6a76bd927203417b
koh-t <koh0927@gmail.com>
2020-07-07 13:57:11 +0900

    web update

--- trends/run.sh
+++ trends/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export GIT_COMMITTER_NAME="koh-t"
+export GIT_COMMITTER_EMAIL="koh0927@gmail.com"
+export GIT_AUTHOR_NAME="koh-t"
+export GIT_AUTHOR_EMAIL="koh0927@gmail.com"
 cd /Users/koh/Dropbox/Proj-web/koh-t.github.io/trends
 /Users/koh/anaconda3/envs/pt_env/bin/python main.py
 git add .
