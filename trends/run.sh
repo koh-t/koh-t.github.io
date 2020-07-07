f85f1cc6b462c77c7624abc9936cdd10ebf30d0c
koh <koh0927@gmail.com>
2020-07-07 13:55:11 +0900

    web update

--- trends/run.sh
+++ trends/run.sh
@@ -3,6 +3,5 @@ cd /Users/koh/Dropbox/Proj-web/koh-t.github.io/trends
 /Users/koh/anaconda3/envs/pt_env/bin/python main.py
 git add .
 git commit -m "web update"
-git config core.sshCommand "ssh -i ~/.ssh/github git push origin master"
 git push origin master
 exit 0
